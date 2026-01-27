0451827a40ab6c67f919c5bdda21d9895c9c7456
tiana-horn <tiana.horn@gmail.com>
2026-01-27 15:23:55 -0500

    Request input from user

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 const version = '0.0.1';
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
@@ -5,10 +7,23 @@ void main(List<String> arguments) {
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
   } else if (arguments.first == 'search') {
-    print('Search command recognized');
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
   } else {
     printUsage();
   }
+}
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+  print('Looking up articles about "$articleTitle". Please wait.');
 }
 
 void printUsage() {
